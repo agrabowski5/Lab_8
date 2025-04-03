6cc0ddd47e70c47194ec00e054fcbf2d81915f6b
Andrew Grabowski <agrabowski5@gmail.com>
2025-04-03 14:53:56 -0400

    Committed!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Biking through the Night</h1>
+<h1>Biking at Night</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div id="map" />
+
+<style>
+	#map {
+		flex: 1;
+	}
+</style>
